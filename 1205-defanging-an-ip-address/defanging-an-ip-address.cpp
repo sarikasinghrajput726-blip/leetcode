0a736351d6848cd4191d56d53f6d65abc17737e4
Sarika Singh <sarikasinghrajput726@gmail.com>
2026-09-05 09:43:38 +0530

    Time: 3 ms (24.81%) | Memory: 7.8 MB (12.79%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string v="";
+        for(int i=0;i<address.length();i++){
+            if(address[i]=='.'){
+                v=v+"[.]";
+            }
+            else{
+                v=v+address[i];
+            }
+        }
+        return v;
+        
+    }
+};
